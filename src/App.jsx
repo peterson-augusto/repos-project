2652b1eeac3692642b4a1185882c83a4dc126233
Peterson <petersonaugusto001@gmail.com>
2024-07-10 13:56:55 -0300

    projeto repos

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Routes from "./routes";
+import GlobalStyle from './styles/global'
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Routes />
+    </>
+  )
+}
---
+++ src/routes.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import Repositorio from "./pages/Repositorio";
+
+export default function RoutesApp() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/repositorio/:repositorio" element={<Repositorio />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
